047406dbe1f8b44c5d1860eb6f77366d2bafb155
motty <motty.mio2@gmail.com>
2025-07-27 23:28:12 +0900

    state のサンプルの更新

--- uart_bridge/example/state.py
+++ uart_bridge/example/state.py
@@ -1,34 +1,36 @@
 import time
-from functools import partial
-
 import zenoh
 
+from uart_bridge.domain.messages import RobotStateId
 from uart_bridge.domain.messages import RobotState
 
 
-class ImageReceiver:
+class StateReceiver:
+    key_expr = "robot/state"
+
     def __init__(self) -> None:
         self.session = zenoh.open(zenoh.Config())
-        key_expr = "robot/state"
 
         for key in RobotState.model_fields.keys():
-            print(f"Subscribing to {key_expr}/{key}")
-            self.session.declare_subscriber(
-                key_expr, partial(self._on_received, key=f"{key_expr}/{key}")
-            )
+            print(f"Subscribing to {self.key_expr}/{key}")
+            self.session.declare_subscriber(f"{self.key_expr}/{key}", self._on_received)
 
-    def _on_received(self, sample: zenoh.Sample, key: str) -> None:
-        print(f"data on {key} received: {sample.payload}")
+    def _on_received(self, sample: zenoh.Sample) -> None:
+        if sample.key_expr == f"{self.key_expr}/state_id":
+            value = RobotStateId(int(sample.payload.to_bytes()))
+        else:
+            value = sample.payload.to_string()
+        print(f"Received {sample.key_expr}: {value}")
 
     def run(self) -> None:
         while True:
-            time.sleep(0.1)
+            time.sleep(1)
 
     def __del__(self) -> None:
         self.session.close()  # type: ignore
 
 
 if __name__ == "__main__":
-    main = ImageReceiver()
+    main = StateReceiver()
 
     main.run()
