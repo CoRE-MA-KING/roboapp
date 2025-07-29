480a64fd64d7007c47399d9e6b8b659b575863b6
motty <motty.mio2@gmail.com>
2025-07-30 00:11:28 +0900

    全部のsubscriber を作成

--- ui_system/src-tauri/src/lib.rs
+++ ui_system/src-tauri/src/lib.rs
@@ -24,6 +24,30 @@ pub fn run() {
         .expect("error while running tauri application");
 }
 
+async fn declare_and_emit(
+    session: &zenoh::Session,
+    app: Arc<AppHandle>,
+    zenoh_key: &str,
+    event_name: &str,
+) {
+    let event_name_cloned = event_name.to_string();
+    session
+        .declare_subscriber(zenoh_key)
+        .callback_mut(move |sample| {
+            app.emit(
+                &event_name_cloned,
+                sample
+                    .payload()
+                    .try_to_string()
+                    .unwrap_or_else(|e| e.to_string().into()),
+            )
+            .unwrap();
+        })
+        .background()
+        .await
+        .unwrap();
+}
+
 async fn zenoh_sub(app: AppHandle) {
     zenoh::init_log_from_env_or("error");
 
@@ -32,46 +56,28 @@ async fn zenoh_sub(app: AppHandle) {
     let session = zenoh::open(config).wait().unwrap();
 
     let app = Arc::new(app);
-    let base_key = "robot/state";
+    let robot_state_key = "robot/state";
+    let robot_command_key = "robot/command";
 
-    {
-        let app = Arc::clone(&app);
-        session
-            .declare_subscriber(format!("{base_key}/pitch_deg"))
-            .callback_mut(move |sample| {
-                app.emit(
-                    "pitch_deg",
-                    sample
-                        .payload()
-                        .try_to_string()
-                        .unwrap_or_else(|e| e.to_string().into()),
-                )
-                .unwrap();
-            })
-            .background()
-            .await
-            .unwrap();
-    }
-    {
-        let app = Arc::clone(&app);
-        session
-            .declare_subscriber(format!("{base_key}/video_id"))
-            .callback_mut(move |sample| {
-                println!("Received video_id: {:?}", sample.payload().try_to_string());
-                app.emit(
-                    "video_id",
-                    sample
-                        .payload()
-                        .try_to_string()
-                        .unwrap_or_else(|e| e.to_string().into()),
-                )
-                .unwrap();
-            })
-            .background()
-            .await
-            .unwrap();
-    }
+    // Robot State
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/state_id"), "video_id").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/pitch_deg"), "pitch_deg").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/muzzle_velocity"), "muzzle_velocity").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/reloaded_left_disks"), "reloaded_left_disks").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/reloaded_right_disks"), "reloaded_right_disks").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/video_id"), "video_id").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/target_panel"), "target_panel").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/auto_aim"), "auto_aim").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/record_video"), "record_video").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/ready_to_fire"), "ready_to_fire").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_state_key}/reserved"), "reserved").await;
+    // Robot Command
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_command_key}/target_x"), "target_x").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_command_key}/target_y"), "target_y").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_command_key}/target_distance"), "target_distance").await;
+    declare_and_emit(&session, Arc::clone(&app), &format!("{robot_command_key}/dummy"), "dummy").await;
 
+    // session.declare_publisher(&format!("{robot_state_key}/request")).await.unwrap().put("request").await.unwrap();
     loop {
         tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
     }
--- ui_system/src/routes/+page.svelte
+++ ui_system/src/routes/+page.svelte
@@ -27,6 +27,23 @@
 </script>
 
 <main class="container">
+	<h2>Image</h2>
 	<ImageViewer {host} {port} />
-	<TextBox pre="Video id is " task_name="video_id" post="!!" />
+
+	<h2>Robot State</h2>
+	<TextBox pre="video_id : " task_name="video_id" post="" />
+	<TextBox pre="pitch_deg : " task_name="pitch_deg" post="" />
+	<TextBox pre="muzzle_velocity : " task_name="muzzle_velocity" post="" />
+	<TextBox pre="reloaded_left_disks : " task_name="reloaded_left_disks" post="" />
+	<TextBox pre="reloaded_right_disks : " task_name="reloaded_right_disks" post="" />
+	<TextBox pre="state_id : " task_name="state_id" post="" />
+	<TextBox pre="target_panel : " task_name="target_panel" post="" />
+	<TextBox pre="auto_aim : " task_name="auto_aim" post="" />
+	<TextBox pre="record_video : " task_name="record_video" post="" />
+	<TextBox pre="ready_to_fire : " task_name="ready_to_fire" post="" />
+
+	<h2>Robot Command</h2>
+	<TextBox pre="target_x : " task_name="target_x" post="" />
+	<TextBox pre="target_y : " task_name="target_y" post="" />
+	<TextBox pre="target_distance : " task_name="target_distance" post="" />
 </main>
