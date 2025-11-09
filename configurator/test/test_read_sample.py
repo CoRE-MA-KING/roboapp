46078f938c3d192e56a97a7bc4f6a26b97638b57
motty <motty.mio2@gmail.com>
2025-11-10 01:13:45 +0900

    ライブラリ化し、テストを追加

---
+++ configurator/test/test_read_sample.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import pytest
+import tomllib
+from configurator.config import Config
+
+
+@pytest.fixture
+def get_resource_path() -> Path:
+    return Path(__file__).parents[2] / "config_sample.toml"
+
+
+def test_read_sample(get_resource_path):
+    with open(get_resource_path, "rb") as f:
+        Config.model_validate(tomllib.load(f))
