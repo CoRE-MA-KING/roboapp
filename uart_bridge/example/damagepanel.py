d90e47de5af65b5a57c71b1fbf7295fd23a535db
motty <motty.mio2@gmail.com>
2025-12-11 21:55:36 +0900

    damagepanel型を扱う

---
+++ uart_bridge/example/damagepanel.py
@@ -0,0 +1,27 @@
+import random
+
+import zenoh
+
+from uart_bridge.domain.messages import DamagePanelRecognition
+
+
+def main() -> None:
+    session = zenoh.open(zenoh.Config())
+    key_expr = "damagepanel"
+
+    # Subscribe to the robot command topic
+
+    pub = session.declare_publisher(f"{key_expr}")
+
+    d = DamagePanelRecognition(
+        target_x=random.randint(0, 1280),
+        target_y=random.randint(0, 720),
+        target_distance=random.randint(0, 100),
+    )
+
+    pub.put(d.model_dump_json())
+    print(f"Published DamagePanelRecognition: {d.model_dump()}")
+
+
+if __name__ == "__main__":
+    main()
