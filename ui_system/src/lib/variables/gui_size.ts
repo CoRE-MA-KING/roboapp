435d0bf01a462934a5ac66490cf9c8933563b12b
motty <motty.mio2@gmail.com>
2025-12-25 00:00:40 +0900

    変数定義

---
+++ ui_system/src/lib/variables/gui_size.ts
@@ -0,0 +1,10 @@
+export const target_width = 1280;
+export const target_height = 720;
+
+export const header_low_height = 100;
+export const header_high_height = 270;
+export const header_side_width = 270;
+
+export const footer_height = 100;
+
+export const footer_left_width = 300;
