0011ae9680c0646bc6ac7fea0c1347b19bfb1994
motty <motty.mio2@gmail.com>
2026-01-02 23:12:37 +0900

    変数を変更

--- ui_system/src/lib/variables/gui_size.ts
+++ ui_system/src/lib/variables/gui_size.ts
@@ -1,10 +1,10 @@
-export const target_width = 1280;
-export const target_height = 720;
-
+// Window
+export const window_width = 1280;
+export const window_height = 720;
+// Header
 export const header_low_height = 100;
 export const header_high_height = 270;
 export const header_side_width = 270;
-
+// Footer
 export const footer_height = 100;
-
 export const footer_left_width = 300;
