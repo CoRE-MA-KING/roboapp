23bfe64861ca66b0fe787edef186f2b921a675ec
motty <motty.mio2@gmail.com>
2026-01-02 20:51:10 +0900

    初期値の方式を変更

--- ui_system/src-tauri/src/msg.rs
+++ ui_system/src-tauri/src/msg.rs
@@ -11,21 +11,25 @@ pub struct DamagePanelRecognition {
 impl Default for DamagePanelRecognition {
     fn default() -> Self {
         Self {
-            target_x: 640,
-            target_y: 360,
-            target_distance: 0,
+            target_x: Self::DEFAULT_TARGET_X,
+            target_y: Self::DEFAULT_TARGET_Y,
+            target_distance: Self::DEFAULT_TARGET_DISTANCE,
         }
     }
 }
 
 impl DamagePanelRecognition {
+    const DEFAULT_TARGET_X: i32 = 640;
+    const DEFAULT_TARGET_Y: i32 = 360;
+    const DEFAULT_TARGET_DISTANCE: i32 = 0;
+
     fn default_target_x() -> i32 {
-        640
+        Self::DEFAULT_TARGET_X
     }
     fn default_target_y() -> i32 {
-        360
+        Self::DEFAULT_TARGET_Y
     }
     fn default_target_distance() -> i32 {
-        0
+        Self::DEFAULT_TARGET_DISTANCE
     }
 }
