78075428ec1db6351f8227b8137df8f9943644f8
motty <motty.mio2@gmail.com>
2026-01-06 01:26:42 +0900

    tauriのrustを構造化する

--- ui_system/src-tauri/src/lib.rs
+++ ui_system/src-tauri/src/lib.rs
@@ -19,7 +19,6 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_log::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
-            state_request,
             get_config_host,
             get_config_port
         ])
@@ -76,12 +75,12 @@ pub fn run() {
 async fn declare_and_emit(
     session: &zenoh::Session,
     app: Arc<AppHandle>,
-    zenoh_key: &str,
+    prefix: &str,
     event_name: &str,
 ) {
     let event_name_cloned = event_name.to_string();
     session
-        .declare_subscriber(zenoh_key)
+        .declare_subscriber(format!("{prefix}{event_name}"))
         .callback_mut(move |sample| {
             app.emit(
                 &event_name_cloned,
@@ -97,30 +96,6 @@ async fn declare_and_emit(
         .unwrap();
 }
 
-#[tauri::command]
-async fn state_request(
-    global_config: State<'_, Mutex<config::GlobalConfig>>,
-) -> Result<(), String> {
-    let prefix = global_config.lock().unwrap().zenoh_prefix.clone();
-
-    log::info!("Requesting robot state");
-    let prefix_slash = if prefix.is_empty() {
-        "".to_string()
-    } else {
-        format!("{prefix}/")
-    };
-    let robot_state_key = format!("{prefix_slash}robot/state/request");
-
-    zenoh_client::create_zenoh_session()
-        .declare_publisher(robot_state_key)
-        .await
-        .map_err(|e| e.to_string())?
-        .put("")
-        .await
-        .map_err(|e| e.to_string())?;
-
-    Ok(())
-}
 
 #[tauri::command]
 fn get_config_host(gui_config: State<'_, Mutex<config::GUIConfig>>) -> Result<String, String> {
@@ -148,119 +123,36 @@ async fn zenoh_sub(app: AppHandle, prefix: String) {
     } else {
         format!("{prefix}/")
     };
-    let robot_state_key = format!("{prefix_slash}robot/state");
 
-    let robot_command_key = format!("{prefix_slash}robot/command");
-
-    // Robot State
     declare_and_emit(
         &session,
         Arc::clone(&app),
-        &format!("{robot_state_key}/state_id"),
-        "video_id",
+        &prefix_slash,
+        "cam/switch",
     )
     .await;
     declare_and_emit(
         &session,
         Arc::clone(&app),
-        &format!("{robot_state_key}/pitch_deg"),
-        "pitch_deg",
+        &prefix_slash,
+        "disks",
     )
     .await;
     declare_and_emit(
         &session,
         Arc::clone(&app),
-        &format!("{robot_state_key}/muzzle_velocity"),
-        "muzzle_velocity",
+        &prefix_slash,
+        "flap",
     )
     .await;
-    declare_and_emit(
+        declare_and_emit(
         &session,
         Arc::clone(&app),
-        &format!("{robot_state_key}/reloaded_left_disks"),
-        "reloaded_left_disks",
-    )
-    .await;
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_state_key}/reloaded_right_disks"),
-        "reloaded_right_disks",
-    )
-    .await;
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_state_key}/video_id"),
-        "video_id",
-    )
-    .await;
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_state_key}/target_panel"),
-        "target_panel",
-    )
-    .await;
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_state_key}/auto_aim"),
-        "auto_aim",
-    )
-    .await;
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_state_key}/record_video"),
-        "record_video",
-    )
-    .await;
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_state_key}/ready_to_fire"),
-        "ready_to_fire",
-    )
-    .await;
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_state_key}/reserved"),
-        "reserved",
-    )
-    .await;
-    // Robot Command
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_command_key}/target_x"),
-        "target_x",
-    )
-    .await;
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_command_key}/target_y"),
-        "target_y",
-    )
-    .await;
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_command_key}/target_distance"),
-        "target_distance",
-    )
-    .await;
-    declare_and_emit(
-        &session,
-        Arc::clone(&app),
-        &format!("{robot_command_key}/dummy"),
-        "dummy",
+        &prefix_slash,
+        "lidar/force_vector",
     )
     .await;
 
-    // session.declare_publisher(&format!("{robot_state_key}/request")).await.unwrap().put("request").await.unwrap();
     loop {
         tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
     }
