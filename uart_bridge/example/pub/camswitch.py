c2fac88c585f0f024c0689e5c235fa580a93f8bc
motty <motty.mio2@gmail.com>
2026-02-01 03:38:37 +0900

    formatter

--- uart_bridge/example/pub/camswitch.py
+++ uart_bridge/example/pub/camswitch.py
@@ -5,12 +5,15 @@ from uart_bridge.domain.transmitter_messages import CameraSwitchMessage
 
 key_expr = "cam/switch"
 
+
 class CameraSwitchPub(ExamplePub):
     def create_message(self) -> CameraSwitchMessage:
         return CameraSwitchMessage(camera_id=random.randint(0, 2))
 
+
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--hz", type=float, help="Publishing frequency in Hz")
     args = parser.parse_args()
