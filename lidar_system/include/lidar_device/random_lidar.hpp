5dfaa1a1a7bab520726661350db5d947e305ee70
motty <motty.mio2@gmail.com>
2026-02-01 18:38:42 +0900

    random algolithmを調整

--- lidar_system/include/lidar_device/random_lidar.hpp
+++ lidar_system/include/lidar_device/random_lidar.hpp
@@ -1,6 +1,7 @@
 #ifndef RANDOM_LIDAR_HPP
 #define RANDOM_LIDAR_HPP
 
+#include <chrono>
 #include <memory>
 #include <random>
 #include <thread>
@@ -9,37 +10,39 @@
 
 class RandomLiDAR : public MockLiDAR {
  private:
-  std::unique_ptr<std::mt19937> random_engine;
-  std::unique_ptr<std::uniform_int_distribution<int>> random_distance;
-  std::unique_ptr<std::uniform_real_distribution<float>> random_rate;
-  std::unique_ptr<std::uniform_real_distribution<float>> random_skip;
+  std::mt19937 random_engine;
+  std::uniform_int_distribution<int> random_distance;
+  std::uniform_real_distribution<float> random_rate;
 
  public:
   inline RandomLiDAR(float max_distance = 1000.0, int32_t min_degree = 0,
                      int32_t max_degree = 360, int32_t rotation = 0)
-      : MockLiDAR(max_distance, min_degree, max_degree, rotation) {
-    random_engine = std::make_unique<std::mt19937>(std::random_device{}());
-    random_distance = std::make_unique<std::uniform_int_distribution<int>>(
-        max_distance * 0.01, max_distance * 0.2);
-    random_rate =
-        std::make_unique<std::uniform_real_distribution<float>>(0.8, 1.2);
+      : MockLiDAR(max_distance, min_degree, max_degree, rotation),
+        random_distance(static_cast<int>(max_distance * 0.2),
+                        static_cast<int>(max_distance * 1.0)),
+        random_rate(0.8, 1.2) {
+    std::random_device rd;
+    auto seed =
+        rd() ^ static_cast<unsigned int>(
+                   std::chrono::system_clock::now().time_since_epoch().count());
+    random_engine.seed(seed);
   };
 
   inline bool get(LiDARDataWrapper &data) override {
-    auto base = (*random_distance)(*random_engine);
+    auto base = random_distance(random_engine);
 
     if (min_degree <= max_degree) {
       for (int degree = min_degree; degree < max_degree; degree++) {
-        int dist = base * (*random_rate)(*random_engine);
+        int dist = base * random_rate(random_engine);
         data.insert((degree + rotation) % 360, dist);
       }
     } else {
       for (int degree = min_degree; degree < 360; degree++) {
-        int dist = base * (*random_rate)(*random_engine);
+        int dist = base * random_rate(random_engine);
         data.insert((degree + rotation) % 360, dist);
       }
       for (int degree = 0; degree < max_degree; degree++) {
-        int dist = base * (*random_rate)(*random_engine);
+        int dist = base * random_rate(random_engine);
         data.insert((degree + rotation) % 360, dist);
       }
     }
