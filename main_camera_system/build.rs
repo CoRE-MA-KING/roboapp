32644f77cb23d5353ccb914d7ddd53d6b291a4a8
motty <motty.mio2@gmail.com>
2026-02-12 00:23:20 +0900

    update mcs build

--- main_camera_system/build.rs
+++ main_camera_system/build.rs
@@ -21,22 +21,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=../utils/proto/");
 
     let proto_include_paths = &["../utils/proto"];
-    let proto_files = &[
-        "../utils/proto/buf/validate/validate.proto",
-        "../utils/proto/roboapp/camera_port.proto",
-        "../utils/proto/roboapp/camera_switch.proto",
-        "../utils/proto/roboapp/damage_panel.proto",
-        "../utils/proto/roboapp/disks.proto",
-        "../utils/proto/roboapp/flap.proto",
-        "../utils/proto/roboapp/lidar_range.proto",
-        "../utils/proto/roboapp/lidar_vector.proto",
-        "../utils/proto/roboapp/robot_state.proto",
-    ];
+    let proto_files: Vec<_> = glob::glob("../utils/proto/**/*.proto")?
+        .filter_map(Result::ok)
+        .collect();
 
     let mut config = prost_build::Config::new();
     config.compile_well_known_types();
 
-    config.compile_protos(proto_files, proto_include_paths)?;
+    config.compile_protos(&proto_files, proto_include_paths)?;
 
     Ok(())
 }
