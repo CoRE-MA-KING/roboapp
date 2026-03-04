2a68726d0a53eedf3b75da99353a96ec548f2565
motty <motty.mio2@gmail.com>
2026-03-04 12:57:46 +0900

    update testcase

--- uart_bridge/test/domain/test_config.py
+++ uart_bridge/test/domain/test_config.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import pytest
-from pydantic import ValidationError
 
 from uart_bridge.domain.config import (
     load_and_parse_config,
@@ -20,19 +19,7 @@ def test_read_empty_uart_config(get_resource_path: Path) -> None:
     assert config.uart is None
 
 
-def test_read_uart_config_with_not_exist(get_resource_path: Path) -> None:
-    config_file = get_resource_path / "uart_device.toml"
-
-    with pytest.raises(ValidationError):
-        load_and_parse_config(config_file)
-
-
 def test_read_uart_config(get_resource_path: Path) -> None:
-    p = Path("/tmp/roboapp_test_uart")
-
-    if not p.exists():
-        p.symlink_to("/dev/null")
-
     config_file = get_resource_path / "uart_device.toml"
 
     c = load_and_parse_config(config_file).uart
@@ -40,7 +27,5 @@ def test_read_uart_config(get_resource_path: Path) -> None:
     if c is None:
         pytest.fail("UART config should not be None")
 
-    assert c.device == "/dev/null"
-
-    if p.exists():
-        p.unlink()
+    # 設定ファイル(uart_device.toml)に記述された "/tmp/roboapp_test_uart" がそのまま読み込まれることを確認
+    assert c.device == "/tmp/roboapp_test_uart"
